206e90ffdef435519dc9b2b6c3d7e7672659cd0f
jgvictores <jcgvicto@gmail.com>
2016-03-10 15:11:58 +0100

    add fmDialogueManager as 2/3 of #3

---
+++ programs/fmDialogueManager/FmDialogueManager.cpp
@@ -0,0 +1,69 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "FmDialogueManager.hpp"
+
+namespace teo
+{
+
+/************************************************************************/
+
+bool FmDialogueManager::configure(yarp::os::ResourceFinder &rf) {
+
+    //ConstString fileName(DEFAULT_FILE_NAME);
+    
+    printf("--------------------------------------------------------------\n");
+    if (rf.check("help")) {
+        printf("FmDialogueManager options:\n");
+        printf("\t--help (this help)\t--from [file.ini]\t--context [path]\n");
+        //printf("\t--file (default: \"%s\")\n",fileName.c_str());
+    }
+    //if (rf.check("file")) fileName = rf.find("file").asString();
+    //printf("FmDialogueManager using file: %s\n",fileName.c_str());
+
+    printf("--------------------------------------------------------------\n");
+    if(rf.check("help")) {
+        ::exit(1);
+    }
+
+    //-----------------OPEN LOCAL PORTS------------//
+    outCmdPort.open("/fmDialogueManager/command:o");
+    outTtsPort.open("/fmDialogueManager/iSpeak:o");
+    inSrPort.open("/fmDialogueManager/speechRecognition:i");
+    stateMachine.setOutCmdPort(&outCmdPort);
+    stateMachine.setOutTtsPort(&outTtsPort);
+    stateMachine.setInSrPort(&inSrPort);
+    while(1){
+        if(outTtsPort.getOutputCount() > 0) break;
+        printf("Waiting for \"/fmDialogueManager/tts:o\" to be connected to something...\n");
+        yarp::os::Time::delay(0.5);
+    }    
+    stateMachine.start();
+    return true;
+}
+
+/************************************************************************/
+double FmDialogueManager::getPeriod() {
+    return 2.0;  // Fixed, in seconds, the slow thread that calls updateModule below
+}
+
+/************************************************************************/
+bool FmDialogueManager::updateModule() {
+    printf("StateMachine in state [%d]. FmDialogueManager alive...\n", stateMachine.getMachineState());
+    return true;
+}
+
+/************************************************************************/
+
+bool FmDialogueManager::interruptModule() {
+    printf("FmDialogueManager closing...\n");
+    inSrPort.interrupt();
+    outTtsPort.interrupt();
+    stateMachine.stop();
+    inSrPort.close();
+    outTtsPort.close();
+    return true;
+}
+
+/************************************************************************/
+
+}  // namespace teo
---
+++ programs/fmDialogueManager/main.cpp
@@ -0,0 +1,54 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/**
+ * 
+ * @ingroup teo_programs
+ * \defgroup fmDialogueManager fmDialogueManager
+ *
+ * @brief Creates an instance of teo::FmDialogueManager.
+ *
+ * @section fmDialogueManager_legal Legal
+ *
+ * Copyright: 2013 (C) Universidad Carlos III de Madrid
+ *
+ * Author: <a href="http://roboticslab.uc3m.es/roboticslab/persona.php?id_pers=72">Juan G. Victores</a>
+ *
+ * CopyPolicy: This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License 3.0 or later
+ *
+ * <hr>
+ *
+ * This file can be edited at fmDialogueManager
+ *
+ */
+
+#include <yarp/os/all.h>
+
+#include "FmDialogueManager.hpp"
+
+
+int main(int argc, char **argv) {
+
+    yarp::os::ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefaultContext("fmDialogueManager");
+    rf.setDefaultConfigFile("fmDialogueManager.ini");
+    rf.configure(argc, argv);
+
+    teo::FmDialogueManager mod;
+    if(rf.check("help")) {
+        return mod.runModule(rf);
+    }
+
+    printf("Run \"%s --help\" for options.\n",argv[0]);
+    printf("%s checking for yarp network... ",argv[0]);
+    fflush(stdout);
+    yarp::os::Network yarp;
+    if (!yarp.checkNetwork()) {
+        fprintf(stderr,"[fail]\n%s found no yarp network (try running \"yarpserver &\"), bye!\n",argv[0]);
+        return -1;
+    } else printf("[ok]\n");
+
+    return mod.runModule(rf);
+}
+
