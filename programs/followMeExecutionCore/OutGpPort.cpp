fe7804aa7d6526a05a34afc6f3dd8df1b3ea8c40
rsantos88 <rasantos@it.uc3m.es>
2016-12-05 12:56:28 +0100

    added new files for -output get position port-

---
+++ programs/followMeExecutionCore/OutGpPort.cpp
@@ -0,0 +1,14 @@
+#include "OutGpPort.hpp"
+
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+namespace teo
+{
+
+/************************************************************************/
+
+void OutGpPort::onRead(Bottle& b) {
+
+}
+
+} // -- namespace teo
---
+++ programs/followMeExecutionCore/OutGpPort.hpp
@@ -0,0 +1,46 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __OUT_GP_PORT_HPP__
+#define __OUT_GP_PORT_HPP__
+
+#include <yarp/os/all.h>
+#include <yarp/dev/all.h>
+#include <stdlib.h>
+
+#define VOCAB_FOLLOW_ME VOCAB4('f','o','l','l')
+#define VOCAB_STOP_FOLLOWING VOCAB4('s','f','o','l')
+
+
+using namespace yarp::os;
+
+namespace teo
+{
+
+/**
+ * @ingroup followMeExecutionCore
+ *
+ * @brief Input port of computer vision data.
+ *
+ */
+class OutGpPort : public BufferedPort<Bottle> {
+    public:
+
+protected:
+        bool follow;
+
+        /** Callback on incoming Bottle. **/
+        virtual void onRead(Bottle& b);
+
+        yarp::dev::IPositionControl *iPositionControl;
+
+        //-- Robot device
+        yarp::dev::IEncoders *iEncoders;
+
+};
+
+}
+
+
+#endif // __OUT_GP_PORT_HPP__
+
+
