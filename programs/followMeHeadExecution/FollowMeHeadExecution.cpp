c02d688bf2c96fcaa0723d2cc7efba4aa738a52c
rsantos88 <rasantos@it.uc3m.es>
2016-12-13 12:08:23 +0100

    Renaming followMeExecutionCore->followMeHeadExecution

---
+++ programs/followMeHeadExecution/FollowMeHeadExecution.cpp
@@ -0,0 +1,85 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "FollowMeHeadExecution.hpp"
+
+namespace teo
+{
+
+/************************************************************************/
+
+bool FollowMeHeadExecution::configure(ResourceFinder &rf) {
+
+    //ConstString fileName(DEFAULT_FILE_NAME);
+    
+    printf("--------------------------------------------------------------\n");
+    if (rf.check("help")) {
+        printf("FollowMeHeadExecution options:\n");
+        printf("\t--help (this help)\t--from [file.ini]\t--context [path]\n");
+        //printf("\t--file (default: \"%s\")\n",fileName.c_str());
+    }
+    //if (rf.check("file")) fileName = rf.find("file").asString();
+    //printf("FollowMeHeadExecution using file: %s\n",fileName.c_str());
+
+    printf("--------------------------------------------------------------\n");
+    if(rf.check("help")) {
+        ::exit(1);
+    }
+
+    //
+    Property headOptions;
+    headOptions.put("device","remote_controlboard");
+    headOptions.put("local","/followMeHeadExecution/head");
+    headOptions.put("remote","/teo/head");
+    headDevice.open(headOptions);
+    if( ! headDevice.isValid() ) {
+        printf("head remote_controlboard instantiation not worked.\n");
+        return false;
+    }
+    if( ! headDevice.view(iPositionControl) ) {
+        printf("view(iPositionControl) not worked.\n");
+        return false;
+    }
+    if( ! headDevice.view(iEncoders) ) {
+        printf("view(iEncoders) not worked.\n");
+        return false;
+    }
+    inCvPort.setIPositionControl(iPositionControl);
+    inSrPort.setIEncoders(iEncoders);
+
+    //-----------------OPEN LOCAL PORTS------------//
+    inSrPort.setInCvPortPtr(&inCvPort);
+    inCvPort.useCallback();
+    inSrPort.useCallback();
+    inSrPort.open("/followMeHeadExecution/dialogueManager/command:i");
+    inCvPort.open("/followMeHeadExecution/cv/state:i");
+
+    return true;
+}
+
+/************************************************************************/
+double FollowMeHeadExecution::getPeriod() {
+    return 2.0;  // Fixed, in seconds, the slow thread that calls updateModule below
+}
+
+/************************************************************************/
+bool FollowMeHeadExecution::updateModule() {
+    //printf("StateMachine in state [%d]. FollowMeHeadExecution alive...\n", stateMachine.getMachineState());
+    return true;
+}
+
+/************************************************************************/
+
+bool FollowMeHeadExecution::interruptModule() {
+    printf("FollowMeHeadExecution closing...\n");
+    inCvPort.disableCallback();
+    inSrPort.disableCallback();
+    inCvPort.interrupt();
+    inSrPort.interrupt();
+    inCvPort.close();
+    inSrPort.close();
+    return true;
+}
+
+/************************************************************************/
+
+}  // namespace teo
---
+++ programs/followMeHeadExecution/InSrPort.cpp
@@ -0,0 +1,42 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "InSrPort.hpp"
+
+namespace teo
+{
+
+/************************************************************************/
+
+void InSrPort::onRead(Bottle& b) {
+    switch ( b.get(0).asVocab() ) {
+        case VOCAB_FOLLOW_ME:
+            printf("follow\n");
+            inCvPortPtr->setFollow(true);
+            break;
+        case VOCAB_STOP_FOLLOWING:
+            printf("stopFollowing\n");
+            inCvPortPtr->setFollow(false);
+            break;
+        case VOCAB_WAVE_APPROPRIATE_HAND:
+            // implementar movimiento brazo dependiendo de iEncoders
+            double encValue;
+            if ( ! iEncoders->getEncoder(0, &encValue) )  // 0 es el tilt del cuello (http://robots.uc3m.es/index.php/TEO_Diagrams)
+            {
+                printf("Error: getEncoder failed\n");
+                return;
+            }
+
+            if(encValue > 0)
+                printf("USER IS ON LEFT -> MOVE THE LEFT ARM\n");
+
+            if(encValue < 0)
+                printf("USER IS ON RIGHT -> MOVE THE RIGHT ARM\n");
+
+        default:
+            break;
+    }
+}
+
+/************************************************************************/
+
+}  // namespace teo
