9da3be5cbef59e76164979ab6128de619831b3a7
rsantos88 <rasantos@it.uc3m.es>
2016-12-16 16:06:46 +0100

    added some changes

--- programs/followMeHeadExecution/InDialoguePortProcessor.cpp
+++ programs/followMeHeadExecution/InDialoguePortProcessor.cpp
@@ -10,23 +10,29 @@ namespace teo
 bool InDialoguePortProcessor::read(ConnectionReader& connection) {
     Bottle in, out;
     double encValue;
-    bool ok = in.read(connection);
+    bool ok = in.read(connection);    
     if (!ok) return false;
+    // -- Gets a way to reply to the message, if possible.
+    ConnectionWriter *returnToSender = connection.getWriter();
 
     switch ( in.get(0).asVocab() ) { //-- b.get(0).asVocab()
         case VOCAB_FOLLOW_ME:
             printf("follow\n");
             inCvPortPtr->setFollow(true);
             break;
+
         case VOCAB_STOP_FOLLOWING:
             printf("stopFollowing\n");
             inCvPortPtr->setFollow(false);
             break;
+
         case VOCAB_WAVE_APPROPRIATE_HAND:           
             if ( ! iEncoders->getEncoder(0, &encValue) )  // 0 es el tilt del cuello (http://robots.uc3m.es/index.php/TEO_Diagrams)
             {
                 printf("Error: getEncoder failed\n");
                 out.addVocab(VOCAB_FAILED);
+                if (returnToSender!=NULL)
+                    out.write(*returnToSender);
                 return true;
             }
             if(encValue > 0)           
@@ -40,15 +46,15 @@ bool InDialoguePortProcessor::read(ConnectionReader& connection) {
             {
                 printf("Error: getEncoder failed\n");
                 out.addVocab(VOCAB_FAILED);
+                if (returnToSender!=NULL)
+                    out.write(*returnToSender);
                 return true;
             }
-            out.addDouble(encValue);
-            break;
 
-        // -- Gets a way to reply to the message, if possible.
-        ConnectionWriter *returnToSender = connection.getWriter();
-        if (returnToSender!=NULL)
-            out.write(*returnToSender);
+            out.addDouble(encValue);
+            if (returnToSender!=NULL)
+                out.write(*returnToSender);
+            break;
 
         return true;
     }
